f667e3e4233900379d41c54c7c97b01252fe8374
Yannick Assogba <yassogba@google.com>
2020-09-29 12:39:57 -0400

    add toxicity demo

--- App.js
+++ App.js
@@ -19,118 +19,36 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-react-native';
-import * as blazeface from '@tensorflow-models/blazeface';
-import * as mobilenet from '@tensorflow-models/mobilenet';
-import * as Permissions from 'expo-permissions';
-import { Camera } from 'expo-camera';
-import {cameraWithTensors, fetch, decodeJpeg} from '@tensorflow/tfjs-react-native';
-
-const TensorCamera = cameraWithTensors(Camera);
-const AUTORENDER = true;
-let frameCount = 0;
-const makePredictionEveryNFrames = 3;
-
-// Position of camera preview.
-const previewLeft = 40;
-const previewTop = 20;
-const previewWidth = 200;
-const previewHeight = 300;
-
-let mobilenetModel;
-const imageUrl = 'https://placekitten.com/224/224';
+import * as toxicity from '@tensorflow-models/toxicity';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isTfReady: false,
-      cameraType: Camera.Constants.Type.front,
-      lastShape: 'none',
-      faces: [],
-      mobilenetClasses: [],
     };
-
-    this.handleImageTensorReady = this.handleImageTensorReady.bind(this);
   }
 
-  async loadBlazefaceModel() {
-    return await blazeface.load();
-  }
-
-  async loadMobileNet() {
-    return await mobilenet.load();
+  async loadToxicityModel() {
+    return await toxicity.load();
   }
 
   async componentDidMount() {    
     await tf.ready();
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    let textureDims;
-    if (Platform.OS === 'ios') {
-      textureDims = { height: 1920,width: 1080 };
-    } else {
-      textureDims = { height: 1200, width: 1600 };
-    }
-    const tensorDims = {height: 300, width: 400 };
-
-    const scale = {
-      height: styles.camera.height / tensorDims.height,
-      width: styles.camera.width / tensorDims.width,
-    }
-
-    const blazefaceModel = await this.loadBlazefaceModel();
-    mobilenetModel = await this.loadMobileNet();    
-    const mobileNetPrediction = await this.makeMobilenetPredictionFromURL(imageUrl);    
+    const toxicityModel = await this.loadToxicityModel(0.9);
+    const modelOutput = await toxicityModel.classify(['you suck','I love you']);
+    console.log('\nmodel output len', modelOutput.length)
+    modelOutput.forEach((classifications) => {
+      console.log('Label', classifications.label);
+      console.log('Results', classifications.results);
+    })
     this.setState({
       isTfReady: true,
-      permissionsStatus: status,
-      faceDetector: blazefaceModel,
-      textureDims,
-      tensorDims,
-      scale,
-      mobilenetClasses: mobileNetPrediction,
+      toxicityModel,
+      modelOutput
     });
   }
 
-  async makeMobilenetPredictionFromURL(url) {    
-    const response = await fetch(url, {}, { isBinary: true });
-    const rawImageData = await response.arrayBuffer();
-    const raw = new Uint8Array(rawImageData)    
-    const imageTensor = decodeJpeg(raw);
-    const pred = await this.makeMobilenetPrediction(imageTensor);    
-    imageTensor.dispose();
-    return pred;
-  }
-
-  async makeMobilenetPrediction(imageTensor) {
-    return await mobilenetModel.classify(imageTensor, 3);
-  }
-
-  async handleImageTensorReady(images) {
-    const loop = async () => {         
-      if (this.state.faceDetector != null) {                
-        if(frameCount % makePredictionEveryNFrames === 0) {
-          const imageTensor = images.next().value;
-          const returnTensors = false;          
-          const faces = await this.state.faceDetector.estimateFaces(
-            imageTensor, returnTensors);
-          tf.dispose(imageTensor);          
-          this.setState({faces});
-        }
-        
-      }
-      frameCount += 1;
-      frameCount = frameCount % makePredictionEveryNFrames;
-      this.rafID = requestAnimationFrame(loop);
-    };
-
-    loop();
-  }
-
-  componentWillUnmount() {
-    if(this.rafID) {
-      cancelAnimationFrame(this.rafID);
-    }
-  }
 
   renderInitialization() {
     return (
@@ -142,87 +60,12 @@ export default class App extends React.Component {
     );
   }
 
-  renderFacesDebugInfo() {
-    const {faces} = this.state;
-    return faces.map((face, i) => {
-      const {topLeft, bottomRight, probability} = face;
-      
-      return <Text key={`faceInfo${i}`}>
-        probability: {probability[0].toFixed(3)} | 
-        TL: [{topLeft[0].toFixed(1)}, {topLeft[1].toFixed(1)}] |
-        BR: [{bottomRight[0].toFixed(1)}, {bottomRight[1].toFixed(1)}]</Text>
-    });
-  }
-
-  renderMobileNetOutput() {
-    const {mobilenetClasses} = this.state;
-    return mobilenetClasses.map((mClass, i) => {
-      const {className, probability} = mClass;      
-      return <Text key={`className${i}`}>
-        className: {className} |
-        probability: {probability.toFixed(3)}
-      </Text>
-    });
-  }
-
   renderMain() {
-    const {textureDims, faces, tensorDims} = this.state;
-
-    const camView = <View style={styles.cameraContainer}>
-      <TensorCamera
-        // Standard Camera props
-        style={styles.camera}
-        type={this.state.cameraType}
-        zoom={0}
-        // tensor related props
-        cameraTextureHeight={textureDims.height}
-        cameraTextureWidth={textureDims.width}
-        resizeHeight={tensorDims.height}
-        resizeWidth={tensorDims.width}
-        resizeDepth={3}
-        onReady={this.handleImageTensorReady}
-        autorender={AUTORENDER}
-      />      
-    </View>;
-
-   
     return (
-      <View>
-        {camView}
-        <Text>tf.version {tf.version_core}</Text>
-        <Text>tf.backend {tf.getBackend()}</Text>
-        <Text># faces detected: {faces.length}</Text>
-        {this.renderBoundingBoxes()}
-        {this.renderFacesDebugInfo()}
-        <Image
-          style={styles.catImage}
-          source={{
-            uri: 'https://placekitten.com/224/224',
-            width: 224,
-            height: 224,
-          }}
-        />
-        {this.renderMobileNetOutput()}
+      <View style={styles.container}>
+        <Text>Model Output!</Text>
       </View>
     );
-  }
-
-  renderBoundingBoxes() {
-    const {faces, scale} = this.state;
-    // On android the bounding boxes need to be mirrored horizontally
-    const flipHorizontal = Platform.OS === 'ios' ? false : true;
-    return faces.map((face, i) => {
-      const {topLeft, bottomRight} = face;
-      const bbLeft = (topLeft[0] * scale.width);      
-      const boxStyle = Object.assign({}, styles.bbox, {
-        left: flipHorizontal ? (previewWidth - bbLeft) - previewLeft :  bbLeft + previewLeft,
-        top: (topLeft[1] * scale.height) + 20,
-        width: (bottomRight[0] - topLeft[0]) * scale.width,
-        height: (bottomRight[1] - topLeft[1]) * scale.height,
-      });
-
-      return <View style={boxStyle} key={`face${i}`}></View>      
-    1});
   }
 
   render() {
@@ -240,34 +83,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  cameraContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    height: '60%',
-    backgroundColor: '#fff',
-  },
-  camera : {
-    position:'absolute',
-    left: previewLeft,
-    top: previewTop,
-    width: previewWidth,
-    height: previewHeight,
-    zIndex: 1,
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 0,
-  },
-  bbox: {
-    position:'absolute',
-    borderWidth: 2,
-    borderColor: 'red',
-    borderRadius: 0,
-  },
-  catImage: {
-    width: 100,
-    height: 100,
-  }
 });
